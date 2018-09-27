15730c30f8b00f0911304768dde1b9a2e9d56a6f
meisteronly <chapinbr@gmail.com>
2018-09-27 10:13:27 -0400

    Initial commit

--- WalkingSimDemo/Assets/NewScript.cs
+++ WalkingSimDemo/Assets/NewScript.cs
@@ -7,6 +7,10 @@ public class NewScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//cooooooooode
+
+		int i = 3;
+		i++;
+		Debug.Log(i);
 	}
 	
 	// Update is called once per frame
